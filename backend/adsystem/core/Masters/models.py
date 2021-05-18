614d2883032a2e73e17718b8c12e17eb1e3e541b
Danilchukd <danilchuk.danil@yandex.ru>
2021-05-18 14:04:24 +0300

    commit

---
+++ backend/adsystem/core/Masters/models.py
@@ -0,0 +1,17 @@
+from adsystem.core.database import db
+
+
+class MasterModel(db.Model):
+    __tablename__ = 'masters'
+    __table_args__ = {'extend_existing': True}
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(50), unique=True, nullable=False)
+    phone = db.Column(db.String(120), nullable=False)
+
+    def jsonify(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'phone': self.phone
+        }
--- backend/adsystem/core/routes.py
+++ backend/adsystem/core/routes.py
@@ -1,9 +1,9 @@
 from starlette.routing import Route, Mount
 from starlette.responses import JSONResponse
 
-from .users.resources import routes as user_routes
-from .roles.resources import routes as roles_routes
-from .permissions.resources import get_apps
+from adsystem.core.users.resources import routes as user_routes
+from adsystem.core.roles.resources import routes as roles_routes
+from adsystem.core.permissions.resources import get_apps
 
 
 async def ping(request):
